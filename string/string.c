a533e1e299e99e378ddc84f89de3c8882f67f636
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-08-19 22:17:30 +0200

    String

---
+++ string/string.c
@@ -0,0 +1,15 @@
+// <?php ob_end_clean(); main();|}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+#define function 
+#define __PHP__ 0
+
+#_C__ int
+function main()
+{
+    $a = "Hello " . "world!";
+    printf("%s", $a);
+    return 0;
+}
