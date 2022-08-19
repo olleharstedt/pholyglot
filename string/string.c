9bb7f335d745226976319366e14f0dfc5c608261
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-08-19 23:24:26 +0200

    Cram it

--- string/string.c
+++ string/string.c
@@ -3,13 +3,11 @@
 #include <glib.h>
 #define function 
 #define __PHP__ 0
-
-#if __PHP__ //<?php
+#if __PHP__//<?php
 class GString { public $str; public function __construct($str) { $this->str = $str; } }
 function g_string_new(string $str) { return new GString($str); }
 function g_string_append(GString $s1, string $s2) { return new GString($s1->str . $s2); }
-#endif //?>
-
+#endif//?>
 //<?php
 #__C__ int
 function main()
@@ -21,6 +19,5 @@ function main()
     printf("%s\n", $b->str);
     return 0;
 }
-
 //?>
 //<?php ob_end_clean(); main();
