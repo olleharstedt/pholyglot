07c5df7922cf8182aaba5f2ed3614e4e418850fc
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-12-01 23:53:03 +0100

    $p = new(Point) works in PHP if you do define('Point', 'Point') first

--- oop/typedef.c
+++ oop/typedef.c
@@ -17,7 +17,7 @@ int main()
     // 22:45 < candide> T *p = malloc(sizeof *p) is more robust than T *p = malloc(sizeof (T)) because sizeof *p will always be the correct size of T and it prevents unnecessarily repeating the T
     // (DRY principle). If sizeof (T) is used and later one of the T's is changed there will be a mismatch. T *p = malloc(sizeof *p) is more concise and easier to read, especially
     // when T is a compound type.
-    Point p = new(Point);
+    Point p = new (Point);
     p->z = 10;
     printf("z = %d\n", p->z);
     return 0;
