feed451a6cd240a57601b22e005c9d1eb1cc5fbf
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-12-11 11:33:16 +0100

    Refactor forth full program test

--- src/lib/test.ml
+++ src/lib/test.ml
@@ -137,8 +137,7 @@ let%test_unit "trivial arith" =
 
 (* Transpile from Pholly AST to polyglot AST *)
 let%test_unit "trivial transpile" =
-    let ast = Ast.Declaration_list [
-        Function {
+    let ast = Ast.Function {
             name = "main";
             docblock = [];
             params = [];
@@ -147,29 +146,16 @@ let%test_unit "trivial transpile" =
             ];
             function_type = Function_type {return_type = Int; arguments = []}
         }
-    ] in
-    let phast = Transpile.run ast in
-    let pholyglot_code = Pholyglot_ast.string_of_program phast in
-    [%test_eq: string] pholyglot_code {|//<?php echo "\x08\x08"; ob_start(); ?>
-#include <stdio.h>
-#include <glib.h>
-#define class struct
-#define __PHP__ 0
-#define new(x) x ## __constructor(alloca(sizeof(struct x)))
-#if __PHP__//<?php
-class GString { public $str; public function __construct($str) { $this->str = $str; } }
-function g_string_new(string $str) { return new GString($str); }
-function g_string_append(GString $s1, string $s2) { return new GString($s1->str . $s2); }
-#endif//?>
-//<?php
-#define function int
+    in
+    let phast = Transpile.declaration_to_pholyglot ast in
+    let pholyglot_code = Pholyglot_ast.string_of_declare phast in
+    [%test_eq: string] pholyglot_code {|#define function int
 function main()
 {
     return 0;
 }
 #undef function
-// ?>
-// <?php ob_end_clean(); main();|}
+|}
 
 let%test_unit "trivial arith transpile" =
     let fn  = 
