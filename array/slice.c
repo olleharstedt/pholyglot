9309782be5695abc5ae60ce257267024211b5cbd
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-12-20 21:07:12 +0100

    Testing uintptr_t in slice

--- array/slice.c
+++ array/slice.c
@@ -4,8 +4,8 @@
 #include <string.h>
 #include <stdint.h>
 #define array(...) {(uintptr_t) __VA_ARGS__}
-#define array_make(type, i, ...) {.thing = (uintptr_t[]) array(__VA_ARGS__), .length = i}
-#define array_get(type, arr, i) ((uintptr_t*) arr.thing)[i]
+#define array_make(i, ...) {.thing = (uintptr_t[]) array(__VA_ARGS__), .length = i}
+#define array_get(type, arr, i) ((type*) arr.thing)[i]
 #define new(x) x ## __constructor(malloc(sizeof(struct x)))
 
 typedef struct Body* Body;
@@ -28,7 +28,7 @@ array array_slice(array old, int offset)
     //printf("%p\n", (void*) ((Body*) old.thing)[0]);
     //printf("%p\n", (void*) ((Body*) old.thing)[1]);
     printf("%d\n", (int) old.length - offset);
-    array new = array_make(Body, old.length - offset, 0);
+    array new = array_make(old.length - offset, 0);
     printf("new length %d\n", (int) new.length);
     int j = 0;
     for (int i = offset; i < old.length; i++) {
@@ -50,7 +50,7 @@ array array_slice(array old, int offset)
  */
 int main()
 {
-    array a = array_make(Body, 2, new(Body), (uintptr_t) new(Body));
+    array a = array_make(2, new(Body), (uintptr_t) new(Body));
     //array a = {.thing = (Body[]) {new(Body), new(Body)}, .length = 2};
     //printf("%p\n", (void*) ((Body*) a.thing)[0]);
     //printf("%p\n", (void*) ((Body*) a.thing)[1]);
@@ -64,6 +64,6 @@ int main()
     };
     */
     array b = array_slice(a, 1);
-    printf("slice b[0]->x = %d\n", ((Body) array_get(Body, b, 0))->x);
+    printf("slice b[0]->x = %d\n", array_get(Body, b, 0)->x);
     return 0;
 }
