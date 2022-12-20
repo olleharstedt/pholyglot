21eb7ed8ffad8e891cb0fc2d4d6faf28b0fb7b5a
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-12-20 23:29:18 +0100

    WIP slice with doubles, longs, Body

--- array/slice.c
+++ array/slice.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#define array(...) {(uintptr_t) __VA_ARGS__}
-#define array_make(i, ...) {.thing = (uintptr_t[]) array(__VA_ARGS__), .length = i}
+#define array(...) {__VA_ARGS__}
+#define array_make(type, i, ...) {.thing = (type[]) array(__VA_ARGS__), .length = i}
 #define array_get(type, arr, i) ((type*) arr.thing)[i]
 #define new(x) x ## __constructor(malloc(sizeof(struct x)))
 
@@ -25,11 +25,15 @@ struct array {
 
 array array_slice(array old, int offset)
 {
-    // TODO: Must have correct length
-    array new = array_make(old.length - offset, 0, 0, 0, 0);
+    size_t new_length = old.length - offset;
+    array new = {
+        .length = new_length,
+        // TODO: Can't make stack alloc here?
+        .thing = (uintptr_t*) malloc(sizeof(uintptr_t) * new_length)
+    };
     int j = 0;
     for (int i = offset; i < old.length; i++) {
-        new.thing[j] = (uintptr_t) old.thing[i];
+        new.thing[j] = old.thing[i];
         j++;
     }
     return new;
@@ -38,10 +42,15 @@ array array_slice(array old, int offset)
 /**
  * gcc -Wall -Werror -pedantic-errors -g slice.c
  * gcc -Wall -Werror -pedantic-errors -Wno-int-conversion -g slice.c
+ * gcc -Wno-incompatible-pointer-types -Wno-int-conversion -g slice.c
  */
 int main()
 {
-    array a = array_make(2, new(Body), new(Body));
+    printf("%ld\n", sizeof(double));
+    printf("%ld\n", sizeof(int));
+    printf("%ld\n", sizeof(uintptr_t));
+    printf("%ld\n", sizeof(Body));
+    array a = array_make(Body, 2, new(Body), new(Body));
     //array a = {.thing = (Body[]) {new(Body), new(Body)}, .length = 2};
     //printf("%p\n", (void*) ((Body*) a.thing)[0]);
     //printf("%p\n", (void*) ((Body*) a.thing)[1]);
@@ -57,9 +66,12 @@ int main()
     array b = array_slice(a, 1);
     printf("slice b[0]->x = %d\n", array_get(Body, b, 0)->x);
 
-    array ints = array_make(3, 11, 22, 33);
-    array ints_slice = array_slice(ints, 1);
-    // TODO: float doesn't work
-    printf("ints slice = %d\n", array_get(int, ints_slice, 0));
+    array ints = array_make(long, 5, 11, 22, 33, 44, 55);
+    array ints_slice = array_slice(ints, 3);
+    printf("ints slice = %ld\n", array_get(long, ints_slice, 0));
+
+    array floats = array_make(double, 3, 1.1, 2.2, 3.3);
+    array floats_slice = array_slice(floats, 2);
+    printf("floats slice = %f\n", array_get(double, floats_slice, 0));
     return 0;
 }
