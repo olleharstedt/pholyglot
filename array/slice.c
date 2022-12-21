05801f981ebc68cab2148541d11723abd55b9a48
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-12-21 20:17:41 +0100

    Slice done

--- array/slice.c
+++ array/slice.c
@@ -7,29 +7,42 @@
 #define array_make(type, i, ...) {.thing = (type[]) array(__VA_ARGS__), .length = i}
 #define array_get(type, arr, i) ((type*) arr.thing)[i]
 #define new(x) x ## __constructor(alloca(sizeof(struct x)))
-
+#define malloc_size(x) alloca(sizeof(uintptr_t) * x)
+#define class struct
+#define _printf printf
 typedef struct Body* Body;
-struct Body {
-    int x;
+//<?php
+class Body {
+#define public int
+#define Body__x $Body__x
+    public $Body__x;
+#undef public
 };
-Body Body__constructor(Body this)
+#if __PHP__
+define("Body", "Body");
+define("long", "int");
+define("double", "float");
+function array_make($class, $length, ...$values) { return $values; }
+function array_get($class, $arr, $i) { return $arr[$i]; }
+function malloc_size() {return null;}
+function _printf($format, ...$args) { fwrite( STDOUT, sprintf( $format, ...$args)); }
+#endif
+//?>
+Body Body__constructor(Body $this)
 {
-    this->x = 10;
-    return this;
+    $this->Body__x = 10;
+    return $this;
 }
 typedef struct array array;
 struct array {
     size_t length;
     uintptr_t* thing;
 };
-
 array array_slice(array old, int offset, uintptr_t* mem)
 {
     size_t new_length = old.length - offset;
     array new = {
         .length = new_length,
-        // TODO: Can't make stack alloc here? Or can, but then it would be copied?
-        // Can pass new memory as argument with alloca
         .thing = mem
     };
     size_t j = 0;
@@ -39,42 +52,49 @@ array array_slice(array old, int offset, uintptr_t* mem)
     }
     return new;
 }
-
 /**
  * gcc -Wall -Werror -pedantic-errors -g slice.c
  * gcc -Wall -Werror -pedantic-errors -Wno-int-conversion -g slice.c
  * gcc -Wno-incompatible-pointer-types -g slice.c
  */
-int main()
+//<?php
+#define function int
+function main()
+#undef function
 {
+    /*
     printf("%ld\n", sizeof(double));
     printf("%ld\n", sizeof(int));
     printf("%ld\n", sizeof(uintptr_t));
     printf("%ld\n", sizeof(Body));
     printf("%ld\n", sizeof(size_t));
-    Body body1 = new(Body);
-    body1->x = 123;
-    array a = array_make(Body, 3, new(Body), new(Body), body1);
-    //array a = {.thing = (Body[]) {new(Body), new(Body)}, .length = 2};
-    //printf("%p\n", (void*) ((Body*) a.thing)[0]);
-    //printf("%p\n", (void*) ((Body*) a.thing)[1]);
-    //printf("%p\n", (void*) ((Body*) a.thing)[2]);
-    //Body body2 = new(Body);
-    /*
-    array a = {
-        .length = 2,
-        .thing = malloc(2 * sizeof(Body))
-    };
     */
-    array b = array_slice(a, 2, alloca(sizeof(uintptr_t) * 2));
-    printf("slice b[0]->x = %d\n", array_get(Body, b, 0)->x);
-
-    array ints = array_make(long, 5, 11, 22, 33, 44, 55);
-    array ints_slice = array_slice(ints, 3, alloca(-1));
-    printf("ints slice = %ld\n", array_get(long, ints_slice, 1));
-
-    array floats = array_make(double, 3, 1.1, 2.2, 3.3);
-    array floats_slice = array_slice(floats, 2, alloca(sizeof(uintptr_t) * 3));
-    printf("floats slice = %f\n", array_get(double, floats_slice, 0));
+    //?>
+    Body //<?php
+    $body1 = new(Body);
+    $body1->Body__x = 123;
+    //?>
+    array //<?php
+    $a = array_make(Body, 3, new(Body), new(Body), $body1);
+    //?>
+    array //<?php
+    $b = array_slice($a, 2, malloc_size(1));
+    _printf("slice b[0]->x = %d\n", array_get(Body, $b, 0)->Body__x);
+    //?>
+    array//<?php
+    $ints = array_make(long, 5, 11, 22, 33, 44, 55);
+    //?>
+    array//<?php
+    $ints_slice = array_slice($ints, 3, malloc_size(2));
+    _printf("ints slice = %ld\n", array_get(long, $ints_slice, 1));
+    //?>
+    array//<?php
+    $floats = array_make(double, 3, 1.1, 2.2, 3.3);
+    //?>
+    array//<?php
+    $floats_slice = array_slice($floats, 2, malloc_size(1));
+    _printf("floats slice = %f\n", array_get(double, $floats_slice, 0));
     return 0;
 }
+//?>
+//<?php main(); ob_end_clean();
