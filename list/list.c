7067ceaee647d988f13f0d478b927cc3f0a5f73c
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-01-24 17:18:03 +0100

    WIP linked list

--- list/list.c
+++ list/list.c
@@ -8,32 +8,69 @@
 typedef struct SplDoublyLinkedList* SplDoublyLinkedList;
 struct SplDoublyLinkedList {
     uintptr_t* item;
-    struct SplDoublyLinkedList* next;
+    struct SplDoublyLinkedList* next_node;
     struct SplDoublyLinkedList* last;
+    struct SplDoublyLinkedList* current_node;
+
     void (*push) (SplDoublyLinkedList self, uintptr_t* item);
+    void (*next) (SplDoublyLinkedList self);
+    uintptr_t* (*current) (SplDoublyLinkedList self);
+    _Bool (*valid) (SplDoublyLinkedList self);
+    void (*rewind) (SplDoublyLinkedList self);
 };
 
 void SplDoublyLinkedList__push(SplDoublyLinkedList self, uintptr_t* item)
 {
-    SplDoublyLinkedList n = malloc(sizeof(struct SplDoublyLinkedList));
-    n->item = item;
-    n->push = &SplDoublyLinkedList__push;
+    if (self->item == NULL) {
+        self->item = item;
+    } else {
+        SplDoublyLinkedList n = malloc(sizeof(struct SplDoublyLinkedList));
+        n->item = item;
 
-    SplDoublyLinkedList current = self;
-    while (current->next != NULL) {
-        current = current->next;
+        SplDoublyLinkedList current = self;
+        while (current->next_node != NULL) {
+            current = current->next_node;
+        }
+
+        current->next_node = n;
+        current->next_node->next_node = NULL;
     }
+}
 
-    current->next = n;
-    current->next->next = NULL;
+uintptr_t* SplDoublyLinkedList__current(SplDoublyLinkedList self)
+{
+    return self->current_node->item;
+}
+
+SplDoublyLinkedList SplDoublyLinkedList__next(SplDoublyLinkedList self)
+{
+    if (self->current_node) {
+        self->current_node = self->current_node->next_node;
+    }
+}
+
+_Bool SplDoublyLinkedList__valid(SplDoublyLinkedList self)
+{
+    return self->current_node != NULL;
+}
+
+void SplDoublyLinkedList__rewind(SplDoublyLinkedList self)
+{
+    self->current_node = self;
 }
 
 SplDoublyLinkedList SplDoublyLinkedList__constructor(SplDoublyLinkedList self)
 {
-    self->push = &SplDoublyLinkedList__push;
-    self->item = NULL;
-    self->last = NULL;
-    self->next = NULL;
+    self->push         = &SplDoublyLinkedList__push;
+    self->current      = &SplDoublyLinkedList__current;
+    self->next         = &SplDoublyLinkedList__next;
+    self->valid        = &SplDoublyLinkedList__valid;
+    self->rewind       = &SplDoublyLinkedList__rewind;
+
+    self->item         = NULL;
+    self->last         = NULL;
+    self->next_node    = NULL;
+    self->current_node = self;
     return self;
 }
 
@@ -43,9 +80,9 @@ typedef struct Point* Point;
 //<?php
 class Point
 {
-#define x $x
+    #define x $x
     public int $x;
-#define y $y
+    #define y $y
     public int $y;
 };
 //?>
@@ -68,7 +105,7 @@ define("Point", "Point");
  */
 #define function int
 function main()
-#undef function
+    #undef function
 {
     #__C__ SplDoublyLinkedList
     $list = new(SplDoublyLinkedList);
@@ -83,22 +120,31 @@ function main()
 
     #__C__ Point
     $p2 = new(Point);
+    $p2->x = 11;
+    $p2->y = 11;
     $list->push(
         #__C__ $list,
         $p2
     );
 
-    #__C__ SplDoublyLinkedList
-    $current = $list;
+    $list->rewind(
+        #__C__ $list
+    );
     do {
-        if ($current->item) {
-            #__C__ Point
-            $tmp = $current->item;
+        #__C__ Point
+        $tmp = $list->current(
+            #__C__ $list
+        );
+        if ($tmp) {
             printf("Current point x = %d\n", $tmp->x);
         }
         printf("Hey\n");
-        $current = $current->next;
-    } while ($current);
+        $list->next(
+            #__C__ $list
+        );
+    } while ($list->valid(
+        #__C__ $list
+    ));
 
     return 0;
 }
--- list/list.php
+++ list/list.php
@@ -4,7 +4,14 @@ define("SplDoublyLinkedList", "SplDoublyLinkedList");
 $list = new(SplDoublyLinkedList);
 $list->push(10);
 $list->push(20);
+$list->push(30);
 
 foreach ($list as $item) {
     echo $item;
 }
+
+for ($i = 0; $i < count($list); $i++) {
+    //echo $list->get($i);
+}
+
+iterator_apply($list, function () { echo 'hej'; });
