93841931c21738aa3acf6d54d69ded9b8e0ffae9
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-02-18 21:20:16 +0100

    WIP infer alloc

--- src/test/pholyglot.ml
+++ src/test/pholyglot.ml
@@ -1,7 +1,16 @@
 open OUnit2
+open Pholyglot.Ast
+open Pholyglot
 
 let tests = "assignment new" >::: [
-    "mo" >:: (fun _ -> assert_equal 1 1)
+    "mo" >:: (fun _ ->
+        let ns = Namespace.create () in
+        let ast = Infer.infer_stmt (Assignment (Infer_me, Variable "p", (New (Class_type ("Point", Infer_allocation_strategy), [])))) ns in
+        assert_equal
+            (Assignment (Infer_me, Variable "p", (New (Class_type ("Point", Infer_allocation_strategy), []))))
+            ast
+        ~printer:Ast.show_statement
+    );
 ]
 
 let _ = run_test_tt_main tests
