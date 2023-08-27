2f4cd63d0038f259fa1ee92b1c066a5bc0aada8c
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-08-27 22:10:16 +0200

    WIP

--- src/lib/test_list.ml
+++ src/lib/test_list.ml
@@ -339,6 +339,7 @@ do {
 (*
 TODO:
     Include Arena in function init (only if arena is used?)
+    Default to Boehm gc if none is given
     Conflicting @var
     Menhir explan:
         /home/olle/.opam/default/bin/menhir lib/parser.mly --base lib/parser --explain
