480de090704ee71d4cf7d752589a831cc0676727
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-09-27 22:25:36 +0200

    WIP

--- anagram/anagram.c
+++ anagram/anagram.c
@@ -3,18 +3,40 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define COMPARE_STRING(res, val) strcmp(res.string, val)
-#define COMPARE_INT(res, val) res.b == val
-#define COMPARE_MIXED(res, val) _Generic(val, \
-    char*: COMPARE_STRING(res, val),\
-    int: COMPARE_INT(res, val)\
-    )(val)
+/**
+21:59 < DPA> olle: Oh, yes, nested generics are a pain. There are a few tricks you may want to know about.
+21:59 < DPA> First of, the semantics of non-matching branches is checked, too, a macro to force the expected type is handy. Someone here came up with this: #define G_FORCE(T, V)
+             _Generic((V), T: (V), default: (T){0}) \n _Generic((X), A: G_FORCE(A, (X)).x, B: G_FORCE(B, (X)).y)
+22:00 < DPA> Secondly, the nested generics all need to match, so consider putting default branches in them. You can use a static_assert and a generic in it resolving to 0 or 1 to make it compile only in the intended cases.
+ */
+
+//#define COMPARE_STRING(res, val) strcmp(res.string.str, val.str)
+//#define COMPARE_INT(res, val) (res.b == val)
+#define COMPARE_MIXED(val) _Generic(val, \
+    struct smartstr: compare_string,\
+    char*: compare_string,\
+    int: compare_int\
+	)
+
+struct smartstr
+{
+    char* str;
+    long len;
+};
 
 union Result
 {
-    char* string;
+    struct smartstr string;
     bool b;
 };
+
+int compare_string(union Result res, struct smartstr val) {
+    return 0;
+}
+
+int compare_int(union Result res, int val) {
+    return 0;
+}
 
 // TODO: Should be able to return false
 char* file_get_contents(char* filename)
@@ -23,9 +45,16 @@ char* file_get_contents(char* filename)
 
 int main()
 {
-    union Result r = {.string = "", .b = true};
-    if (COMPARE_MIXED(&r, true)) {
-        printf("Hello\n");
+    //union Result r;
+    //r.b = true;
+    //if (COMPARE_MIXED(r, true)) {
+        //printf("Hello\n");
+    //}
+    union Result r2;
+    //r2.string = {.str = "moo", .len = 3};
+    struct smartstr s2 = {.str = "moo", .len = 3};
+    if (COMPARE_MIXED(s2)) {
+        printf("Hello 2\n");
     }
     return 0;
 }
---
+++ anagram/generictricks.c
@@ -0,0 +1,44 @@
+#include <limits.h>
+#include <stdio.h>
+
+#define G_FORCE(T, V) _Generic((V), T: (V), default: (T){0})
+
+struct A {
+  int x;
+};
+
+struct B {
+  float y;
+};
+
+void mycrazyfunction_A_A(int a, int b){ printf("AA %d %d\n",a,b); }
+void mycrazyfunction_A_B(int a, float b){ printf("AB %d %f\n",a,b); }
+void mycrazyfunction_B_A(float a, int b){ printf("BA %f %d\n",a,b); }
+
+#define mycrazyfunction(X,Y) _Generic((X), \
+    struct A: _Generic((Y), \
+      struct A: mycrazyfunction_A_A(G_FORCE(struct A, (X)).x, G_FORCE(struct A, (Y)).x), \
+      struct B: mycrazyfunction_A_B(G_FORCE(struct A, (X)).x, G_FORCE(struct B, (Y)).y), \
+      default: (void)0 \
+    ), \
+    struct B: _Generic((Y), \
+      struct A: mycrazyfunction_B_A(G_FORCE(struct B, (X)).y, G_FORCE(struct A, (Y)).x), \
+      default: (void)0 \
+    ), \
+    default: (void)(struct{ int x; static_assert( \
+      _Generic((X), \
+        struct A: _Generic((Y), struct A: 1, struct B: 1, default: 0), \
+        struct B: _Generic((Y), struct A: 1, default: 0), \
+        default: 0 \
+      ) \
+    , "This type combination isn't supported");}){0} \
+  )
+
+int main(){
+  struct A a = {42};
+  struct B b = {3.14};
+  mycrazyfunction(a,a);
+  mycrazyfunction(a,b);
+  mycrazyfunction(b,a);
+  // mycrazyfunction(b,b); // This one would trigger the assert
+}
