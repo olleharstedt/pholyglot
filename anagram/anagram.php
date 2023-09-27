f8d05540f16f15d8c17bb84d4e38916104fcab93
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-09-27 20:55:22 +0200

    Add notes about C/PHP compat

--- anagram/anagram.php
+++ anagram/anagram.php
@@ -1,15 +1,36 @@
 <?php
 
+// strtok
+// fgets
 $words = explode("\n", file_get_contents('http://wiki.puzzlers.org/pub/wordlists/unixdict.txt'));
 foreach ($words as $word) {
+    // string to array, but string is already array in C
     $chars = str_split($word);
+    // strsort, qsort
     sort($chars);
+    // implode, but string is already an array
+    // $anagram is SplDoublyLinkedList<String>
     $anagram[implode($chars)][] = $word;
 }
 
+// No stdlib map
+/*
+int *map(int *array, int size, int (*fn)(int)) {
+  int *new_array = malloc(size * sizeof(int));
+  for (int i = 0; i < size; i++) {
+    new_array[i] = fn(array[i]);
+  }
+  return new_array;
+}
+*/
+// fmax from math.h
+// define("count", "count");
+// array_map(count, $anagram) might work if count() is already define, and array_map expect proper function pointer
+// But can use void* function pointers too
 $best = max(array_map('count', $anagram));
 foreach ($anagram as $ana) {
     if (count($ana) == $best) {
+        // Do something else than print_r here
         print_r($ana);
     }
 }
